df933e3152992afa00ebbe9f23ae55be98491b92
atakanakyildizz <atakanmembership@gmail.com>
2022-11-21 19:55:03 +0100

    My initial code

--- LAB7/[07.1.6] Ordered list.py
+++ LAB7/[07.1.6] Ordered list.py
@@ -1,12 +1,15 @@
-#[07.1.6] Ordered list
+# [07.1.6] Ordered list
 
 import random
 
 my_list = []
+
+
 def random20number(a):
     for i in range(20):
-        a.append(random.randint(0,99))
+        a.append(random.randint(0, 99))
         a = sorted(a)
     print(a)
+
 
 random20number(my_list)
--- LAB7/[07.2.1] Measurement noise.py
+++ LAB7/[07.2.1] Measurement noise.py
@@ -1,2 +1,32 @@
 # [07.2.1] Measurement noise
+import random
 
+
+def random20number(a):
+    for i in range(20):
+        a.append(random.randint(0, 99))
+    return a
+
+
+def mean_of_the_list(name_of_list):
+    total = 0
+    for i in range(len(name_of_list)):
+        total = total + name_of_list[i]
+    return total/len(name_of_list)
+
+
+def measurment_noise(the_list,mean_of_the_list):
+    for i in range(len(the_list)):
+        the_list[i] = mean_of_the_list
+    return the_list
+def main():
+    my_list = []
+    my_list = random20number(my_list)
+
+    print(f"List: {my_list}\n"
+          f"Length: {len(my_list)}\n"
+          f"Mean of list: {mean_of_the_list(my_list)}\n"
+          f"My final list is {measurment_noise(my_list,mean_of_the_list(my_list))}")
+
+
+main()
