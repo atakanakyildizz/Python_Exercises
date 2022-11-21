70659b2a6db9012312099c48bb686064d01f3ff4
atakanakyildizz <atakanmembership@gmail.com>
2022-11-21 12:18:10 +0100

    My initial code

---
+++ LAB7/7.1.1.py
@@ -0,0 +1,17 @@
+
+a = [1,4,9,16,9,7,4,9,11]
+b = []
+
+for i in range(len(a)):
+    if (i%2) == 1:
+        b.append((-1*a[i]))
+    else:
+        b.append(a[i])
+
+    total = 0
+for i in range(len(b)):
+    total = total + b[i]
+
+
+print(f"list = {b}")
+print(f"total is = {total}")
