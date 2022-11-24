86b9e15b85e9cd2734507e51fb21742e3c7d0952
atakanakyildizz <atakanmembership@gmail.com>
2022-11-25 00:13:56 +0100

    My initial code

--- LAB5/[2.3] Predator-Prey Simulation.py
+++ LAB5/[2.3] Predator-Prey Simulation.py
@@ -1,7 +1,13 @@
 # [2.3] Predator-Prey Simulation
 
 def main():
-    pass
+    A, B, C, D = 0.1, 0.01, 0.01, 0.00002
+    x0, y0 = 1000, 20
+    for i in range(100):
+        x1 = x0 * (1 + A - B * y0)
+        y1 = y0 * (1 - C + D * x0)
+        x0, y0 = x1, y1
+        print(int(x1), int(y1))
 
 
 if __name__ == '__main__':
---
+++ LAB5/[2.4] Electrical transformers.py
@@ -0,0 +1,6 @@
+# [2.4] Electrical transformers
+
+R0, Vs, n, Rs, = 20, 40, 1, 8
+
+Ps = Rs * ((n*Vs) / ((n**2)*(R0)+Rs))**2
+print(Ps)
