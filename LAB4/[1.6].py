c1a45c5b6a44b583cae6cb8b8988205c03b28afb
atakanakyildizz <atakanmembership@gmail.com>
2022-11-26 22:46:31 +0100

    My initial code

--- LAB4/[1.6].py
+++ LAB4/[1.6].py
@@ -1,7 +1,33 @@
 # [1.6]
+number = 30
+
 
 def main():
-    pass
+    first_n_prime_number(number)
+
+
+def first_n_prime_number(my_number):
+    my_list = []
+    for i in range(2, my_number):
+        if check_prime(i):
+            my_list.append(i)
+    print(my_list)
+
+
+def check_prime(my_number):
+    counter = 0
+    numbers = []
+    for i in range(2, my_number):
+        if my_number % i == 0:
+            counter += 1
+            numbers.append(i)
+    if counter == 0:
+        solution = True
+
+    else:
+        solution = False
+
+    return solution
 
 
 if __name__ == '__main__':
