c13cd47be4f272e3b1dc0f92675cc56bac1a0e61
atakanakyildizz <atakanmembership@gmail.com>
2022-11-26 22:54:32 +0100

    My initial code

--- LAB4/[1.7].py
+++ LAB4/[1.7].py
@@ -1,8 +1,18 @@
 # [1.7]
 
 
+def the_question(my_string):
+
+    for i in range(len(my_string)):
+        print(my_string[i])
+    for i in range(len(my_string[1:3])):
+        print(my_string[i:i+2])
+    for i in range(len(my_string[1:2])):
+        print(my_string[i:i+3])
+
+
 def main():
-    pass
+    the_question("rum")
 
 
 if __name__ == '__main__':
