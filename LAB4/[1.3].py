3a9e82cf13ac2b7ee7651e52f3e8d1bd28e82743
atakanakyildizz <atakanmembership@gmail.com>
2022-11-26 22:28:14 +0100

    My initial code

--- LAB4/[1.3].py
+++ LAB4/[1.3].py
@@ -1,7 +1,25 @@
 # [1.3]
+n = 4
+
+
+def first_question(x):
+#   x = int(input(""Enter the n value: ))
+    for a in range(x):
+        print(f"{'*' * x}")
+
+
+def second_question(x):
+    for a, b in zip(range(x, 0, -1), range(x)):
+        print(f"{(a-1)*' '}{b*'*'}*{b*'*'}")
+
+    for c, d in zip(range(1, x), range(x-2, -1, -1)):
+        print(f"{c*' '}{d*'*'}*{d*'*'}")
+
 
 def main():
-    pass
+    first_question(n)
+    print("\n")
+    second_question(n)
 
 
 if __name__ == '__main__':
---
+++ LAB4/[1.5].py
@@ -0,0 +1,8 @@
+# [1.5]
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
