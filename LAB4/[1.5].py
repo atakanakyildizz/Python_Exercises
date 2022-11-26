ef73f3d629a01809594dabeb5c4278141140650e
atakanakyildizz <atakanmembership@gmail.com>
2022-11-26 22:40:28 +0100

    My initial code

--- LAB4/[1.5].py
+++ LAB4/[1.5].py
@@ -1,7 +1,22 @@
 # [1.5]
 
 def main():
-    pass
+    number = int(input("Enter the number that you want to check: "))
+    (check_prime(number))
+
+
+def check_prime(number):
+    counter = 0
+    numbers = []
+    for i in range(2, number):
+        if number % i == 0:
+            counter += 1
+            numbers.append(i)
+    if counter == 0:
+        print("Prime number")
+    else:
+        print("Not A Prime Number")
+        print(numbers)
 
 
 if __name__ == '__main__':
