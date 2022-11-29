95158627f167187bd61eef04a908abb78a49be18
atakanakyildizz <atakanmembership@gmail.com>
2022-11-29 16:07:17 +0100

    My initial code

--- LAB8/tic_tac_toe.py
+++ LAB8/tic_tac_toe.py
@@ -1,13 +1,10 @@
-import random
-from pprint import pprint
-
 
 def make_empty_table(row, column):
-    A = [[[] for i in range(row)] for i in range(column)]
+    a = [[[] for i in range(row)] for i in range(column)]
     for i in range(row):
         for j in range(column):
-            A[i][j] = '_'
-    return A
+            a[i][j] = '_'
+    return a
 
 
 def print_table(row, column, table):
@@ -55,42 +52,69 @@ def second_player_move(table):
 
 
 def check_winning(table):
-    define = True
-    if table[0][0] == table[0][1] == table[0][2] or \
-            table[0][1] == table[1][1] == table[2][1] or \
-            table[0][2] == table[1][2] == table[2][2] or \
-            table[0][0] == table[1][0] == table[2][0] or \
-            table[0][1] == table[1][1] == table[2][1] or \
-            table[0][2] == table[1][2] == table[2][2] or \
-            table[0][0] == table[1][1] == table[2][2] or \
-            table[0][2] == table[1][1] == table[2][0]:
-            define = False
-    return define
+    my_return = False
+    if (table[0][0]) == "X" or "x":
+        if table[0][0] == table[0][1] == table[0][2] or table[0][0] == table[1][1] == table[2][2] or \
+                table[0][0] == table[1][0] == table[2][0] or table[0][0] == table[1][0] == table[2][0]:
+            my_return = True
+
+    elif table[0][1] == 'X' or "x":
+        if table[0][1] == table[1][1] == table[2][1] or table[0][1] == table[1][1] == table[2][1]:
+            my_return = True
+
+    elif table[0][2] == 'x' or 'X':
+        if table[0][2] == table[1][2] == table[2][2] or \
+                table[0][2] == table[1][2] == table[2][2] or table[0][2] == table[1][1] == table[2][0]:
+            my_return = True
+
+    elif (table[0][0]) == "O" or "o":
+        if table[0][0] == table[0][1] == table[0][2] or table[0][0] == table[1][1] == table[2][2] or \
+                table[0][0] == table[1][0] == table[2][0] or table[0][0] == table[1][0] == table[2][0]:
+            my_return = True
+
+    elif table[0][1] == 'O' or "o":
+        if table[0][1] == table[1][1] == table[2][1] or table[0][1] == table[1][1] == table[2][1]:
+            my_return = True
+
+    elif table[0][2] == 'O' or 'o':
+        if table[0][2] == table[1][2] == table[2][2] or\
+                table[0][2] == table[1][2] == table[2][2] or table[0][2] == table[1][1] == table[2][0]:
+            my_return = True
+
+
+    return my_return
+
+
+def spaces_in_table(table):
+    counter = 0
+    my_return = False
+    for i in range(3):
+        for j in range(3):
+            if table[i][j] == "_":
+                counter = counter + 1
+    return counter
 
 
 def game():
     table = (make_empty_table(3, 3))
-    first_player_move(table)
-    second_player_move(table)
 
-    for i in range(9):
+    while True:
         first_player_move(table)
         print_table(3, 3, table)
-        if check_winning(table) == False:
-            print("First player win ")
+        if check_winning(table) == True:
+            print("First player win\n ")
             break
-        for a in table:
-            if a != ' ':
-                break
-                break
+        if spaces_in_table(table) == 0:
+            print("Its draw ")
+            break
         second_player_move(table)
-        if check_winning(table) == False:
-            print("Second player win ")
+        print_table(3, 3, table)
+        if check_winning(table) == True:
+            print("Second player win\n ")
             break
-        for a in table:
-            if a != ' ':
-                break
-                break
+        if spaces_in_table(table) == 0:
+            print("Its draw ")
+            break
 
 
 def main():
